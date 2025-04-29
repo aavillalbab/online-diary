0ca5d4c4510de219c09386940722f2a62829f078
Arith Alexis Villalba Bravo <arith.villalba@insoftar.com>
2025-04-29 07:47:11 -0500

    add router for admin user

--- src/Controller/Admin/NewsAdminController.php
+++ src/Controller/Admin/NewsAdminController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/secured/admin/news', name: 'admin_news_')]
+#[Route('/news', name: 'admin_news_')]
 class NewsAdminController extends AbstractController
 {
 
