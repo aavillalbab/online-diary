06e6caf28eabf966316ccfa16d0c40644d4f2b19
Arith Alexis Villalba Bravo <arith.villalba@insoftar.com>
2025-04-29 14:21:22 -0500

    add dsf-file-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     dsarhoya\BaseBundle\dsarhoyaBaseBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    dsarhoya\DSYFilesBundle\dsarhoyaDSYFilesBundle::class => ['all' => true],
 ];
